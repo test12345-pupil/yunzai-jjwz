a7a0674fe4391404bfd9b043c29880d80201321e
xzz-233 <xuzhouzi@qq.com>
2023-12-12 13:17:18 +0800

    修改随机咖波的性能bug

--- 随机咖波.js
+++ 随机咖波.js
@@ -12,22 +12,22 @@ const ext_images = {
   webp: 1,
 }
 
+
+let random_image_data = {}
+fs.readdirSync(random_image_path).forEach(file => {
+  if(fs.lstatSync(random_image_path + file).isDirectory()){
+      let list_images = fs.readdirSync(random_image_path + file)
+          .filter(fileName => extname(fileName).slice(1) in ext_images);
+      if(list_images.length > 0){
+        random_image_data[file] = list_images;
+        console.log(`成功加载表情文件夹：${file}，共${list_images.length}个表情`);
+      }
+  }
+});
+
+
 export class random_capoos extends plugin {
     constructor(e) {
-      let random_image_data = {}
-      fs.readdirSync(random_image_path).forEach(file => {
-        if(fs.lstatSync(random_image_path + file).isDirectory()){
-            let list_images = fs.readdirSync(random_image_path + file)
-                .filter(fileName => extname(fileName).slice(1) in ext_images);
-            if(list_images.length > 0){
-              random_image_data[file] = list_images;
-              console.log(`成功加载表情文件夹：${file}，共${list_images.length}个表情`);
-            }
-        }
-      });
-      
-      if(!random_image_data) return;
-
       super({
         name: "随机咖波",
         dsc: "随机咖波",
@@ -41,16 +41,15 @@ export class random_capoos extends plugin {
         ],
       });
 
-      this.random_image_data = random_image_data
+      random_image_data = random_image_data
     }
   
     async randomImage(){
       const ImageSetName = this.e.msg.replace(/^(#)?随机/, "");
-      console.log(ImageSetName);
-      if(this.random_image_data[ImageSetName]){
-        let len = this.random_image_data[ImageSetName].length;
+      if(random_image_data[ImageSetName]){
+        let len = random_image_data[ImageSetName].length;
         const message = [
-          segment.image(`${random_image_path}${ImageSetName}/${this.random_image_data[ImageSetName][Math.floor(Math.random()*len)]}`)
+          segment.image(`${random_image_path}${ImageSetName}/${random_image_data[ImageSetName][Math.floor(Math.random()*len)]}`)
         ]
         this.e.reply(message);
       }
